457d7b6d41c019602a1374c7d72fe5bc0d8f3b95
Pangpond220 <62050220@kmitl.ac.th>
2022-03-11 22:42:34 +0700

    change icon

--- Project/Game1.cs
+++ Project/Game1.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AstroFight.States;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Media;
-using AstroFight.States;
-using AstroFight.Controls;
+using System.Collections.Generic;
 
 
 namespace AstroFight
